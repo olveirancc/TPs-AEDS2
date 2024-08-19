aad457ed2678745e745a0c6fec2f5cfc4061f715
olveiriancc <jorge.amado.ncc@gmail.com>
2024-08-19 18:33:49 -0300

    ainda com erro

--- TP1/q02/Q2.c
+++ TP1/q02/Q2.c
@@ -1,26 +1,37 @@
 #include <stdio.h>
 #include <string.h>
 
-
-int Palindromo (char entrada[]){
-    for(int i = 0; i <= strlen(entrada)/2; i++){
-        if(entrada[i] != entrada[strlen(entrada) - i - 1]) return 0;
+int Palindromo(char entrada[])
+{
+    int tam = strlen(entrada);
+    for (int i = 0; i < tam / 2; i++)
+    {
+        if (entrada[i] != entrada[tam - i - 1])
+            return 0;
     }
     return 1;
 }
 
-int Fim (char entrada[]){
-    if(entrada[0] == 'F' && entrada[1] == 'I' && entrada[2] == 'M'){
-        return 1;
+int Fim(char entrada[])
+{
+    if (entrada[0] == 'F' && entrada[1] == 'I' && entrada[2] == 'M')
+    {
+        return 0;
     }
-    else return 0;
+    else
+        return 1;
 }
 
-int main (void) {
-    char entrada[200];
+int main(void)
+{
+    char entrada[500];
     scanf("%[^\n]%*c", entrada);
-    while(!(Fim(entrada))){
-        if(Palindromo(entrada)) printf("SIM\n");
+    while (Fim(entrada))
+    {
+        if (Palindromo(entrada))
+        {
+            printf("SIM\n");
+        }
         else printf("NAO\n");
         scanf("%[^\n]%*c", entrada);
     }
